c97fe69c6b1cd2dc32286611658203402abffaf1
Alex Shuvalov <ashuvduddits@gmail.com>
2021-12-19 10:49:42 +0300

    dz1 completed

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,30 @@
-import {sum} from "./index";
+import {solve, sum} from "./index";
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('x^2+1 = 0 no quadratic roots', () => {
+  expect(solve(1, 0, 1)).toStrictEqual([]);
+});
+
+test('x^2-1 = 0 there are 2 roots of multiplicity 1 (x1=1, x2=-1)', () => {
+  expect(solve(1, 0, -1)).toStrictEqual([1, -1]);
+});
+
+test('x^2+2x+1 = 0 there is one root of multiplicity 2 (x1 = x2 = -1)', () => {
+  expect(solve(1, 2, 1)).toStrictEqual([-1, -1]);
+});
+
+test('x^2-2.00000001x+1 = 0 there is one root of multiplicity 2 (x1 = x2 = 2)', () => {
+  expect(solve(1, -2.00000001, 1)).toStrictEqual([1.000000005, 1.000000005]);
+});
+
+test('a !== 0', () => {
+  expect(() => solve(1e-7, 1, 1)).toThrow('a не может быть равно 0');
+});
+
+test('parameters typeof number', () => {
+  // @ts-ignore
+  expect(() => solve('0', 1, 1)).toThrow('Параметры должны иметь тип "number"');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 export const sum = (a: number, b: number) => a+b;
+
+export const solve = (a: number, b: number, c: number, e: number = 1e-5): number[] => {
+  if ((typeof a !== 'number') || (typeof b !== 'number') || (typeof c !== 'number')) {
+    throw new Error('Параметры должны иметь тип "number"');
+  }
+  if (Math.abs(a) < e) {
+    throw new Error('a не может быть равно 0');
+  }
+  const D = Math.pow(b, 2) - 4 * a * c;
+  if (D < -e) {
+    return [];
+  }
+  if (Math.abs(D) <= e) {
+    return [-b/(2*a), -b/(2*a)];
+  }
+  if (D > e) {
+    return [-b + Math.sqrt(D)/(2*a), -b - Math.sqrt(D)/(2*a)];
+  }
+  return [];
+}
