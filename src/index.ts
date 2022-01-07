10ee58d38e0a385ecc82fb114d21b6c2acbf054b
Alex Shuvalov <ashuvduddits@gmail.com>
2022-01-07 16:29:52 +0300

    fix test 7

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export const sum = (a: number, b: number) => a+b;
+export const sum = (a: number, b: number) => a + b;
 
 export const solve = (a: number, b: number, c: number, e: number = 1e-5): number[] => {
-  if ((typeof a !== 'number') || (typeof b !== 'number') || (typeof c !== 'number')) {
+  if ((typeof a !== 'number') || (typeof b !== 'number') || (typeof c !== 'number') || isNaN(a) || isNaN(b) || isNaN(c) || !isFinite(a) || !isFinite(b) || !isFinite(c)) {
     throw new Error('Параметры должны иметь тип "number"');
   }
   if (Math.abs(a) < e) {
@@ -12,10 +12,10 @@ export const solve = (a: number, b: number, c: number, e: number = 1e-5): number
     return [];
   }
   if (Math.abs(D) <= e) {
-    return [-b/(2*a), -b/(2*a)];
+    return [-b / (2 * a), -b / (2 * a)];
   }
   if (D > e) {
-    return [-b + Math.sqrt(D)/(2*a), -b - Math.sqrt(D)/(2*a)];
+    return [-b + Math.sqrt(D) / (2 * a), -b - Math.sqrt(D) / (2 * a)];
   }
   return [];
 }
